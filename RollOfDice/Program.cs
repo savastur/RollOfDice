a7a8d22970715a9148885b779f103db959a40690
Sturm <savagesturm@gmail.com>
2026-02-28 17:55:14 -0700

    Fixed the number count and added comments explaining how it works

--- RollOfDice/Program.cs
+++ RollOfDice/Program.cs
@@ -6,62 +6,41 @@ namespace RollOfDice
     {
         static void Main(string[] args)
         {
-            string[] numberList = { "1", "2", "3", "4", "5", "6" };
+            int[] rolls = new int[11];
+            //Random Number Generator
+            Random sixSidedDie = new Random();
+            for (int rollDice = 0; rollDice < 1000; rollDice++)
+            {
+                //Rolls two dice and adds together
+                int sixSidedNumber = sixSidedDie.Next(1,7) + sixSidedDie.Next(1,7); //(min , max "not inclusive")
+                //incremnts every time an number is rolled and removes the zero's displayed for 0 and 1
+                rolls[sixSidedNumber - 2]++;
+            }
+                Display(rolls);
+            //pause
+            Console.Read();
+        }
+        static void Display(int[] numberSum)
+        {
+            //Number roll array
+            string[] numberList = { "2", "3", "6", "7", "8", "9", "10", "11", "12" };
+            //Write number array
             foreach (string numberindex in numberList)
             {
                 Console.Write(numberindex.PadLeft(2) + " |");
 
             }
+            //Carriage Return
             Console.WriteLine();
-            Random sixSidedDie = new Random();
-            for (int rollDice = 0; rollDice < 1000; rollDice++)
+            //Write the total each number that's bee rolled
+            foreach (int sum in numberSum)
             {
-                int sixSidedNumber = sixSidedDie.Next(1,7); //(min , max "not inclusive")
-                NumberTracker(sixSidedNumber); //sixSidedNumber = number in NumberTracker
+                Console.WriteLine("Amount of time's numbers were rolled.");
+                Console.Write(sum.ToString().PadLeft(2) + " |");
+
             }
-            //pause
-            Console.Read();
         }
-        static void NumberTracker(int number)
-        {
-            int firstNumber = 0;
-            int secondNumber = 0;
-            int thirdNumber = 0;
-            int fourthNumber = 0;
-            int fifthNumber = 0;
-            int sixthNumber = 0;
-            if (number == 1)
-            {
-                ++firstNumber;
-            }
-            if (number == 2)
-            {
-                ++secondNumber;
-            }
-            if (number == 3)
-            {
-                ++thirdNumber;
-            }
-            if (number == 4)
-            {
-                ++fourthNumber;
-            }
-            if (number == 5)
-            {
-                ++fifthNumber;
-            }
-            if (number == 6)
-            {
-                ++sixthNumber;
-            }
-            string[] accumlatedNumbers = { $"{firstNumber}", $"{secondNumber}", $"{thirdNumber}", $"{fourthNumber}", $"{fifthNumber}", $"{sixthNumber}" };
-            foreach(string sum in accumlatedNumbers)
-            {
-                Console.Write(sum.PadLeft(2) + " |");
-                Console.Clear();
-            }
-            
-         
-        }
+        
+       
     }
 }
